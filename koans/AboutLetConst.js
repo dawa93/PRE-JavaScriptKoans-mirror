9d1d0fe91c6c2333eeddb28f25ddc1e8b90b7acd
Hong Shik Kim <kimploo@gmail.com>
2020-08-04 02:16:07 +0900

    fix error

--- koans/AboutLetConst.js
+++ koans/AboutLetConst.js
@@ -3,17 +3,18 @@ describe('`const`에 대해서', function () {
     const constNum = 0;
     constNum = 0; // TODO : delete me
     expect(constNum).toBe(0);
+
     const constString = 'I am a const';
     constString = "which means I'm a constant variable, delete me."; // TODO
     expect(constString).toBe('I am a const');
   });
+
   it('const로 선언해도 배열의 요소에 접근할 수 있습니다.', function () {
     const arr = [];
 
     const toBePushed = FILL_ME_IN; // TODO
-    const pushed = arr.push(toBePushed);
+    arr.push(toBePushed);
 
-    expect(pushed).toBe(42);
     expect(arr).toEqual([42]);
   });
   it('const로 선언해도 객체의 속성을 추가 및 삭제할 수 있습니다.', function () {
