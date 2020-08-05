cc89ac3e6f045832c7613ae4b135f009e7491c64
dawa93 <dawa93@naver.com>
2020-08-05 13:50:53 +0900

    Completed AdvancedAboutArrayMethod.js

--- koans/Advanced-AboutAdvancedArrayMethods.js
+++ koans/Advanced-AboutAdvancedArrayMethods.js
@@ -1,84 +1,84 @@
-describe('Array Method에 관하여', function () {
-  it("'filter' method에 관해 학습합니다.", function () {
+describe("Array Method에 관하여", function() {
+  it("'filter' method에 관해 학습합니다.", function() {
     let numbers = [1, 2, 3];
 
-    let odd = numbers.filter(function (x) {
+    let odd = numbers.filter(function(x) {
       return x % 2 !== 0;
     });
 
-    expect(odd).toEqual(FILL_ME_IN);
-    expect(odd.length).toBe(FILL_ME_IN);
-    expect(numbers.length).toBe(FILL_ME_IN);
+    expect(odd).toEqual([1, 3]);
+    expect(odd.length).toBe(2);
+    expect(numbers.length).toBe(3);
   });
 
-  it("'map' method에 관해 학습합니다.", function () {
+  it("'map' method에 관해 학습합니다.", function() {
     let numbers = [1, 2, 3];
 
-    let numbersPlus1 = numbers.map(function (x) {
+    let numbersPlus1 = numbers.map(function(x) {
       return x + 1;
     });
 
-    expect(numbersPlus1).toEqual(FILL_ME_IN);
-    expect(numbers).toEqual(FILL_ME_IN);
+    expect(numbersPlus1).toEqual([2, 3, 4]);
+    expect(numbers).toEqual([1, 2, 3]);
   });
 
-  it("'reduce' method에 관해 학습합니다.", function () {
+  it("'reduce' method에 관해 학습합니다.", function() {
     let numbers = [1, 2, 3];
-    let reduction = numbers.reduce(function (memo, x) {
+    let reduction = numbers.reduce(function(memo, x) {
       return memo + x;
     }, 0);
 
-    expect(reduction).toBe(FILL_ME_IN);
-    expect(numbers).toEqual(FILL_ME_IN);
+    expect(reduction).toBe(6);
+    expect(numbers).toEqual([1, 2, 3]);
   });
 
-  it("'forEach' method에 관해 학습합니다.", function () {
+  it("'forEach' method에 관해 학습합니다.", function() {
     let numbers = [1, 2, 3];
     let result = [];
 
-    let isEven = function (item) {
+    let isEven = function(item) {
       result.push(item % 2 === 0);
     };
 
     numbers.forEach(isEven);
 
-    expect(result).toEqual(FILL_ME_IN);
-    expect(numbers).toEqual(FILL_ME_IN);
+    expect(result).toEqual([false, true, false]);
+    expect(numbers).toEqual([1, 2, 3]);
   });
 
-  it("'every' method에 관해 학습합니다.", function () {
+  it("'every' method에 관해 학습합니다.", function() {
     let onlyEven = [2, 4, 6];
     let mixedBag = [2, 4, 5, 6];
 
-    let isEven = function (x) {
+    let isEven = function(x) {
       return x % 2 === 0;
     };
 
-    expect(onlyEven.every(isEven)).toBe(FILL_ME_IN);
-    expect(mixedBag.every(isEven)).toBe(FILL_ME_IN);
+    expect(onlyEven.every(isEven)).toBe(true);
+    expect(mixedBag.every(isEven)).toBe(false);
   });
 
-  it("'some' method에 관해 학습합니다.", function () {
+  it("'some' method에 관해 학습합니다.", function() {
     let onlyEven = [2, 4, 6];
     let mixedBag = [2, 4, 5, 6];
 
-    let isEven = function (x) {
+    let isEven = function(x) {
       return x % 2 === 0;
     };
 
-    expect(onlyEven.some(isEven)).toBe(FILL_ME_IN);
-    expect(mixedBag.some(isEven)).toBe(FILL_ME_IN);
+    expect(onlyEven.some(isEven)).toBe(true);
+    expect(mixedBag.some(isEven)).toBe(true);
   });
 
-  it('2개의 method를 연속적으로 사용하는 방법에 관해 학습합니다.', function () {
+  it("2개의 method를 연속적으로 사용하는 방법에 관해 학습합니다.", function() {
     let result = [0, 1, 2]
-      .map(function (x) {
+      .map(function(x) {
         return x + 1;
       })
-      .reduce(function (sum, x) {
+      .reduce(function(sum, x) {
         return sum + x;
       });
 
-    expect(result).toEqual(FILL_ME_IN);
+    expect(result).toEqual(6);
   });
 });
